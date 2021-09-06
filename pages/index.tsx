d3c0f42f8340eadfc6ff63cf4cfae4b5d62335cf
batidodecoco <ariel.pacciaroni@wedevelop.me>
2021-09-06 20:38:58 -0300

    Layout structure

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@material-ui/core'
+import { ReactElement } from 'react'
+
+export default function Index (): ReactElement {
+  return (
+    <div>
+      <Button color='primary'>Hola mundo</Button>
+    </div>
+  )
+}
